022916ca220c31325c4f14f3369a9b2584a910f2
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2014-09-26 17:13:55 +0800

    Try to boost by sieve on factorization

--- 251.py
+++ 251.py
@@ -1,57 +1,122 @@
-import math
+import itertools
 import sys
+
+class PrimeTable():
+    def __init__(self, bound):
+        self.factorization = None
+        self.primes = None
+        self._sieve(bound)
+    
+    def _sieve(self, bound):
+        self.factorization = [{} for _ in range(bound + 1)]
+        self.primes = []
+        visited = [False] * (bound + 1)
+        visited[0] = visited[1] = True
+        for i in range(2, bound + 1):
+            if visited[i]:
+                continue
+            self.primes.append(i)
+            for j in range(i, bound + 1, i):
+                visited[j] = True
+                self.factorization[j][i] = 1
+            d = i**2
+            while d <= bound:
+                for j in range(d, bound + 1, d):
+                    self.factorization[j][i] += 1
+                d *= i
+        print('Sieve completed:', len(self.primes))
+
+class Factorization():
+    def __init__(self, bound):
+        self.prime_table = PrimeTable(bound)
+    
+    # Sieve?
+    
+    def factorize(self, n):
+        return self.prime_table.factorization[n]
+        
+        d = n
+        rv = {}
+        for i in range(len(self.prime_table.primes)):
+            p = self.prime_table.primes[i]
+            if d == 1 or p > d:
+                break
+            e = 0
+            while d % p == 0:
+                d = d // p
+                e += 1
+            if e > 0:
+                rv[p] = e
+        if d > 1:
+            rv[d] = 1
+        return rv
+    
+    def get_all_divisor(self, factorization):
+        unpacking = [[p**e for e in range(factorization[p] + 1)] for p in factorization]
+        return [self._product(divisor) for divisor in itertools.product(*unpacking)]
+
+    def get_problem251(self, k):
+        first_part = self.factorize(k + 1) # (k + 1)^2
+        second_part = self._get_square_divisor(8 * k + 5) # (8k + 5)
+        return self._merge_two_factorization(first_part, second_part)
+
+    def _get_square_divisor(self, n):
+        rv = {}
+        d = n
+        for i in range(len(self.prime_table.primes)):
+            p = self.prime_table.primes[i]
+            p_square = p**2
+            if p_square > d:
+                break
+            e = 0
+            while d % p_square == 0:
+                d = d // p_square
+                e += 1
+            if e > 0:
+                rv[p] = e
+        return rv
+
+    def _merge_two_factorization(self, x, y):
+        result = dict(x)
+        for p in y:
+            if p not in result:
+                result[p] = 0
+            result[p] += y[p]
+        return result
+
+    def _product(self, list):
+        result = 1
+        for number in list:
+            result *= number
+        return result
 
 class Problem():
     def __init__(self):
-        pass
+        self.factorization = None
 
     def solve(self):
-        self.get_count(1000)
-        #self.get_count_benchmark(1000)
-    
+        self.get_count(1000000)
+        #self.get_count(110000000)
+
     def get_count(self, bound):
+        max_k = (bound - 2)//3
+        self.factorization = Factorization(max_k + 1)
+
         count = 0
         for k in range((bound - 2)//3 + 1):
-            a = 2 + 3*k
-            x = (1 + 2*k)**3 + (2 + 3*k)**2 # where x = b^2 c
+            a = 3*k + 2
+            x = (k + 1)**2 * (8 * k + 5) # where x = b^2 c
             if 4 * (bound - a)**3 < 27 * x:
                 break
-            max_b = min(bound + 1 - a, self._int_sqrt(x))
-            for b in range(1, max_b + 1):
-                c, r = divmod(x, b**2)
-                if r == 0 and a + b + c <= bound:
+            x_good_factorization = self.factorization.get_problem251(k)
+            for b in self.factorization.get_all_divisor(x_good_factorization):
+                c = x // b**2
+                if a + b + c <= bound:
                     count += 1
-                    print(count, "=>", a, b, c)
-
-    def get_count_benchmark(self, bound):
-        count = 0
-        for k in range((bound-2)//3 + 1):
-            a = 2 + 3*k
-            for b in range(1, bound + 1 - a):
-                for c in range(1, bound + 1 - a - b):
-                    if self.is_cardano_triplet(a, b, c):
-                        count += 1
+                    if count % 1000 == 0:
                         print(count, "=>", a, b, c)
-
-    def is_cardano_triplet(self, a, b, c):
-        sum = 0
-        for x in [a + b * math.sqrt(c), a - b * math.sqrt(c)]:
-            if x >= 0:
-                sum += x**(1/3)
-            else:
-                sum -= (-x)**(1/3)
-        return abs(sum - 1) < 1e-9
-
-    def _int_sqrt(self, n):
-        guess = (n >> n.bit_length() // 2) + 1
-        result = (guess + n // guess) // 2
-        while abs(result - guess) > 1:
-            guess = result
-            result = (guess + n // guess) // 2
-        while result * result > n:
-            result -= 1
-        return result
-
+        print(count)
+        return count
 
 def main():
     problem = Problem()
