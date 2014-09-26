591817686fcaf74d7cbe3d0ad6f42db7e78b4773
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2014-09-26 18:10:50 +0800

    Try to sieve (8k+5)

--- 251.py
+++ 251.py
@@ -3,13 +3,12 @@ import sys
 
 class PrimeTable():
     def __init__(self, bound):
-        self.factorization = None
-        self.primes = None
+        self.factorization = [{} for _ in range(bound + 1)]
+        self.special_factorization = [{} for _ in range(bound + 1)]
+        self.primes = []
         self._sieve(bound)
     
     def _sieve(self, bound):
-        self.factorization = [{} for _ in range(bound + 1)]
-        self.primes = []
         visited = [False] * (bound + 1)
         visited[0] = visited[1] = True
         for i in range(2, bound + 1):
@@ -24,14 +23,44 @@ class PrimeTable():
                 for j in range(d, bound + 1, d):
                     self.factorization[j][i] += 1
                 d *= i
+    
+            # Sieve on (8k+5)-type number
+            if i == 2 or 8 * bound + 5 < i**2:
+                continue
+            k = (-5 * self._mod_inverse(8, i**2)) % i**2
+            if k > bound:
+                continue
+            for j in range(k, bound + 1, k):
+                self.special_factorization[j][i] = 1
+            d = i**4
+            while d <= bound:
+                k = (-5 * self._mod_inverse(8, d)) % d
+                for j in range(k, bound + 1, k):
+                    self.special_factorization[j][i] += 1
+                d *= i**2
+        print(self.factorization)
+        print(self.special_factorization)
         print('Sieve completed:', len(self.primes))
+
+    def _mod_inverse(self, a, mod):
+        g, x, y = self._extended_gcd(a, mod)
+        if g != 1:
+            raise Exception('modular inverse does not exist')
+        else:
+            return x % mod
+
+    def _extended_gcd(self, a, b):
+        if a == 0:
+            return (b, 0, 1)
+        else:
+            g, y, x = self._extended_gcd(b % a, a)
+            return (g, x - (b // a) * y, y)
 
 class Factorization():
     def __init__(self, bound):
         self.prime_table = PrimeTable(bound)
     
     # Sieve?
-    
     def factorize(self, n):
         return self.prime_table.factorization[n]
         
@@ -57,10 +86,14 @@ class Factorization():
 
     def get_problem251(self, k):
         first_part = self.factorize(k + 1) # (k + 1)^2
-        second_part = self._get_square_divisor(8 * k + 5) # (8k + 5)
+        second_part = self._get_square_divisor(k) # (8k + 5)
         return self._merge_two_factorization(first_part, second_part)
 
-    def _get_square_divisor(self, n):
+    # Sieve?
+    def _get_square_divisor(self, k):
+        # buggy!
+        # return self.prime_table.special_factorization[k]
+        n = 8 * k + 5
         rv = {}
         d = n
         for i in range(len(self.prime_table.primes)):
@@ -95,7 +128,7 @@ class Problem():
         self.factorization = None
 
     def solve(self):
-        self.get_count(1000000)
+        self.get_count(1000)
         #self.get_count(110000000)
 
     def get_count(self, bound):
