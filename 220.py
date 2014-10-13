65b8540b581594082054ed2edd9e769355b59cfd
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2014-10-13 13:13:52 +0800

    Find patterns

--- 220.py
+++ 220.py
@@ -27,38 +27,26 @@ class HeighwayDragon():
         return spot
 
 class Problem():
-    def __init__(self):
-        self.spot_lg_map = {}
-        self.spot_lg_map[0] = [0, 1]
-        for i in range(1, 100):
-            self.spot_lg_map[i] = [
-                    self.spot_lg_map[i-1][0] + self.spot_lg_map[i-1][1],
-                    self.spot_lg_map[i-1][1] - self.spot_lg_map[i-1][0]
-            ]
-
     def solve(self):
         self.benchmark()
-        #print(self.get(50, 10**12))
+        for step in [500, 10**12]:
+            print(step, '=>', self.get(step))
     
     def benchmark(self):
         heighway_dragon = HeighwayDragon(10)
-        for step in [22]:
-            print(step, heighway_dragon.simulate(step), self.get(10, step), "{0:b}".format(step))
-
-    def get(self, order, step):
-        assert(step % 2 == 0)
-        bin_rep = "{0:b}".format(step)
-        bin_rep_len = len(bin_rep)
-        spot = list(self.spot_lg_map[bin_rep_len - 1])
-        for i in range(1, bin_rep_len - 1):
-            if bin_rep[i] == "1":
-                print(spot, self.spot_lg_map[bin_rep_len - i - 1])
-                spot[0] -= self.spot_lg_map[bin_rep_len - i - 1][0]
-                spot[1] -= self.spot_lg_map[bin_rep_len - i - 1][1]
-            #else:
-            #    spot[0] -= self.spot_lg_map[bin_rep_len - i - 1][0]
-            #    spot[1] -= self.spot_lg_map[bin_rep_len - i - 1][1]
-        return spot
+        print(heighway_dragon.simulate(500))
+    
+    def get(self, step):
+        spot = [0, 1]
+        curr_step = 1
+        while curr_step < step:
+            spot = [spot[0] + spot[1], -spot[0] + spot[1]]
+            curr_step *= 2
+        if curr_step == step:
+            return spot
+        else:
+            rest_spot = self.get(curr_step - step)
+            return [spot[0] - rest_spot[1], spot[1] + rest_spot[0]]
 
 def main():
     problem = Problem()
