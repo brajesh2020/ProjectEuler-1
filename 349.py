529d8d4bdbadbfd42e7934f54ece5dd4880b0ea9
Meng-Gen Tsai <menggen@thermalmix-lm.(none)>
2014-10-17 20:24:09 +0800

    Find patterns

--- 349.py
+++ 349.py
@@ -1,7 +1,34 @@
-class LangtonAntSimulator():
+class LangtonAnt():
     def __init__(self):
         self.bound = 120
+        self.black_count_range = 11209
+        self.black_count_map = [0 for _ in range(self.black_count_range)]
+        self._init_black_count()
     
+    def _init_black_count(self):
+        board = [[True for _ in range(self.bound)] for _ in range(self.bound)]
+        ant_pos = [self.bound * 3 // 4, self.bound // 2]
+        ant_direction = [1, 0]
+        for i in range(self.black_count_range):
+            x, y = ant_pos
+            assert(x >= 0 and x < self.bound and y >= 0 and y < self.bound)
+            if board[x][y] is True:
+                board[x][y] = False
+                ant_direction = [ant_direction[1], -ant_direction[0]]
+                ant_pos = [ant_pos[0] + ant_direction[0], ant_pos[1] + ant_direction[1]]
+            else:
+                board[x][y] = True
+                ant_direction = [-ant_direction[1], ant_direction[0]]
+                ant_pos = [ant_pos[0] + ant_direction[0], ant_pos[1] + ant_direction[1]]
+            self.black_count_map[i] = self.count_black(board)
+
+    def get_black_count(self, step):
+        if step >= 1 and step <= self.black_count_range:
+            return self.black_count_map[i]
+        common_diff = self.black_count_map[11208] - self.black_count_map[11000]
+        q, r = divmod(step - 1 - 11000, 208)
+        return self.black_count_map[11000 + r] + common_diff * q
+
     def simulate(self, step=12000):
         board = [[True for _ in range(self.bound)] for _ in range(self.bound)]
         ant_pos = [self.bound * 3 // 4, self.bound // 2]
@@ -46,8 +73,8 @@ class LangtonAntSimulator():
 
 class Problem():
     def solve(self):
-        simulator = LangtonAntSimulator()
-        simulator.simulate()
+        ant = LangtonAnt()
+        print(ant.get_black_count(10**18))
 
 def main():
     problem = Problem()
