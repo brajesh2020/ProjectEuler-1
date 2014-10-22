3c4695c3dc60d2f427008d7e75fe7d4cbdee89f0
Meng-Gen <plover@gmail.com>
2014-10-22 23:45:31 +0800

    TODO: remove bendable foldings to reduce search space

--- 300.py
+++ 300.py
@@ -7,38 +7,45 @@ class Folding():
         dfs_stack = [initial_state]
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         while dfs_stack:
-            top = dfs_stack[-1]
+            top_folding, top_turn = dfs_stack[-1]
             dfs_stack.pop()
-            if self.__is_overlapping(top):
+            if self.__is_overlapping(top_folding):
                 continue
-            if len(top[0]) == dim:
-                vaild_foldings.append(top[0])
-                if len(vaild_foldings) % 1000 == 0:
-                    print("vaild path", len(vaild_foldings), top[0])
+            if len(top_folding) == dim:
+                if not self.__is_bendable(top_folding):
+                    vaild_foldings.append(top_folding)
+                #if len(vaild_foldings) % 1000 == 0:
+                print("vaild path", len(vaild_foldings), top_folding)
                 continue
-            last_x, last_y = top[0][-1]
+            last_x, last_y = top_folding[-1]
             for direction_x, direction_y in directions:
-                next_turn = top[1]
-                if top[1] is False:
+                next_turn = top_turn
+                if top_turn is False:
                     if direction_x == -1 and direction_y == 0:
                         continue
                     elif direction_x == 1 and direction_y == 0:
                         next_turn = True
                 next_spot = (last_x + direction_x, last_y + direction_y)
-                next_state = (top[0] + [next_spot], next_turn)
+                next_state = (top_folding + [next_spot], next_turn)
                 dfs_stack.append(next_state)
                 #print(next_state)
         return vaild_foldings
 
-    def __is_overlapping(self, state):
-        folding = state[0]
+    def __is_overlapping(self, folding):
         visited_spots = set([str(_) for _ in folding])
         return len(folding) != len(visited_spots)
+
+    def __is_bendable(self, folding):
+        N = len(folding)
+        for i in range(1, N-1):
+            print(folding[i])
+        raise NotImplementedError()
+        return False
 
 class Problem():
     def solve(self):
         folding = Folding()
-        vaild_foldings = folding.build(15)
+        vaild_foldings = folding.build(4)
         #print(vaild_foldings)
 
 def main():
