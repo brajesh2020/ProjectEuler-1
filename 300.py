8b0cab834493fd9eb4c8c128f3e23ee4a4d2beda
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2014-10-23 17:34:37 +0800

    Brute force

--- 300.py
+++ 300.py
@@ -1,8 +1,10 @@
+from ast import literal_eval
+import itertools
 import sys
 
 class Folding():
     def build(self, dim):
-        vaild_foldings = []
+        vaild_contacts = set()
         initial_state = ([(0, 0), (0, 1)], False)
         dfs_stack = [initial_state]
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -12,10 +14,8 @@ class Folding():
             if self.__is_overlapping(top_folding):
                 continue
             if len(top_folding) == dim:
-                if not self.__is_bendable(top_folding):
-                    vaild_foldings.append(top_folding)
-                #if len(vaild_foldings) % 1000 == 0:
-                print("vaild path", len(vaild_foldings), top_folding)
+                contact = self.__get_contact(top_folding)
+                vaild_contacts.add(str(contact))
                 continue
             last_x, last_y = top_folding[-1]
             for direction_x, direction_y in directions:
@@ -28,25 +28,67 @@ class Folding():
                 next_spot = (last_x + direction_x, last_y + direction_y)
                 next_state = (top_folding + [next_spot], next_turn)
                 dfs_stack.append(next_state)
-                #print(next_state)
-        return vaild_foldings
+        return [literal_eval(_) for _ in vaild_contacts]
 
     def __is_overlapping(self, folding):
         visited_spots = set([str(_) for _ in folding])
         return len(folding) != len(visited_spots)
 
-    def __is_bendable(self, folding):
-        N = len(folding)
-        for i in range(1, N-1):
-            print(folding[i])
-        raise NotImplementedError()
-        return False
+    def __get_contact(self, folding):
+        rv = []
+        n = len(folding)
+        for i in range(n):
+            for j in range(i + 1, n):
+                distance = abs(folding[i][0] - folding[j][0]) + abs(folding[i][1] - folding[j][1])
+                if distance == 1:
+                    rv.append(2**i + 2**j)
+        return sorted(rv)
 
 class Problem():
     def solve(self):
+        self.get(15)
+    
+    def get(self, dim):
         folding = Folding()
-        vaild_foldings = folding.build(4)
-        #print(vaild_foldings)
+        contacts = folding.build(dim)
+        print("contact search space size =>", len(contacts))
+        
+        count_map = {}
+        protein_count = 0
+        for protein, reversed_protein in self.__protein_iterator(dim):
+            if protein in count_map:
+                continue
+            count = 0
+            for contact in contacts:
+                curr_count = 0
+                for point in contact:
+                    if protein & point == point:
+                        curr_count += 1
+                count = max(count, curr_count)
+            count_map[protein] = count
+            count_map[reversed_protein] = count
+            protein_count += 1
+            print(protein_count, protein, "=>", count)
+        total_count = 0
+        for protein in count_map:
+            total_count += count_map[protein]
+        print(total_count)
+        print(self.__format_number(total_count * 5**dim, dim))
+
+    def __protein_iterator(self, n):
+        for protein in range(2**n):
+            reversed_protein = sum(1<<(n-1-i) for i in range(n) if protein>>i & 1)
+            yield protein, reversed_protein
+
+    def __format_number(self, number, pos):
+        rep = str(number)
+        if len(rep) > pos:
+            rep = rep[:len(rep) - pos] + "." + rep[len(rep) - pos:]
+        else:
+            rep = "0." + "".join(["0" for i in range(pos - len(rep))]) + rep
+        while rep[-1] == "0":
+            rep = rep[:-1]
+        return rep
 
 def main():
     problem = Problem()
