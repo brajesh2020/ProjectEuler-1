8bb878877cc99789d06a20313fb25925ba0ad2a1
Meng-Gen Tsai <menggen@thermalmix-lm.tpcity.corp.yahoo.com>
2015-04-10 17:21:53 +0800

    Log internal computing

--- 403.py
+++ 403.py
@@ -1,19 +1,16 @@
-from fractions import gcd
 import sys
 
 class Problem():
     def solve(self):
-        print([self.get(n) for n in range(1, 100 + 1)])
-        #for n in range(1, 10):
-        #    print(n, '=>', self.get(n))
-        #self.get(5)
-        #self.get(100)
+        self.get(10)
+        #print([self.get(n) for n in range(1, 10 + 1)])
 
     def get(self, n):
         squares = {}
         for i in range(n + 2):
             squares[i**2] = i
-        #print('squares =>', squares)
+
+        x_map = {}
 
         total_sum = 0
         for a in range(-n, n + 1):
@@ -22,12 +19,20 @@ class Problem():
                 if discriminant in squares:
                     x_0 = (a - squares[discriminant]) // 2
                     x_1 = (a + squares[discriminant]) // 2
+
+                    if x_0 not in x_map:
+                        x_map[x_0] = []
+                    x_map[x_0].append(x_1)
+
                     lattice_points = self.count_lattice_points(x_0, x_1)
                     total_sum += lattice_points
-                    #print(a, b, '=>', x_0, x_1, lattice_points)
-        return total_sum
-        #print('total_sum =>', total_sum)
+                    print(a, b, '=>', x_0, x_1, lattice_points)
+        print('total_sum =>', total_sum)
 
+        print(x_map)
+
+        return total_sum
+        
     def count_lattice_points(self, x_0, x_1):
         line = self.count_lattice_points_under_line(x_0, x_1)
         parabola = self.count_lattice_points_under_parabola(x_0, x_1)
@@ -35,11 +40,7 @@ class Problem():
         return line - parabola
 
     def count_lattice_points_under_line(self, x_0, x_1):
-        y_0, y_1 = x_0**2, x_1**2
-        double_area = (y_0 + y_1) * (x_1 - x_0) 
-        boundary_points = y_0 + y_1 + 2 * (x_1 - x_0) 
-        double_interior_points = double_area + 2 - boundary_points
-        return boundary_points + double_interior_points // 2
+        return (x_1 - x_0 + 1) * (x_0**2 + x_1**2 + 2) // 2
 
     def count_lattice_points_under_parabola(self, x_0, x_1):
         if x_0 >= 0:
