fe3f36232af9617b56a76862a7852ddc2508fbdc
Meng-Gen <plover@gmail.com>
2015-04-13 00:10:36 +0800

    prepare helper function

--- 403.py
+++ 403.py
@@ -41,6 +41,54 @@ class Problem():
         return total_sum
 
 
+    def count_under_line(self, x_0_first, x_0_last, x_1_first, x_1_last):
+        x_0_sigma_list = self.sigma_list(x_0_first, x_0_last)
+        x_1_sigma_list = self.sigma_list(x_1_first, x_1_last)
+        double_count = - x_0_sigma_list[3] * x_1_sigma_list[0] \
+                + x_0_sigma_list[2] * x_1_sigma_list[1] \
+                + x_0_sigma_list[2] * x_1_sigma_list[0] \
+                - x_0_sigma_list[1] * x_1_sigma_list[2] \
+                - 2 * x_0_sigma_list[1] * x_1_sigma_list[0] \
+                + x_0_sigma_list[0] * x_1_sigma_list[3] \
+                + x_0_sigma_list[0] * x_1_sigma_list[2] \
+                + 2 * x_0_sigma_list[0] * x_1_sigma_list[1] \
+                + 2 * x_0_sigma_list[0] * x_1_sigma_list[0]
+        return double_count // 2
+
+    def count_under_parabola(self, x_0_first, x_0_last, x_1_first, x_1_last):
+        assert(x_0_last <= x_1_first)
+        x_0_sigma_list = self.sigma_list(x_0_first, x_0_last)
+        x_1_sigma_list = self.sigma_list(x_1_first, x_1_last)
+        if x_0_first >= 0:
+            six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
+                    + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
+                    + x_0_sigma_list[0] * x_1_sigma_list[1] \
+                    - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
+                    + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
+                    - x_0_sigma_list[1] * x_1_sigma_list[0]
+            return six_times_count // 6
+        elif x_0_last < 0:
+            if x_1_first >= 0:
+                six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
+                        + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
+                        + x_0_sigma_list[0] * x_1_sigma_list[1] \
+                        - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
+                        + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
+                        - x_0_sigma_list[1] * x_1_sigma_list[0]
+                return six_times_count // 6
+            elif x_1_last < 0:
+                six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
+                        + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
+                        + x_0_sigma_list[0] * x_1_sigma_list[1] \
+                        - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
+                        + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
+                        - x_0_sigma_list[1] * x_1_sigma_list[0]
+                return six_times_count // 6
+            else:
+                raise NotImplementedError()
+        else:
+            raise NotImplementedError()
+
 
     def count_lattice_points(self, x_0, x_1):
         line = self.count_lattice_points_under_line(x_0, x_1)
@@ -51,14 +99,32 @@ class Problem():
         return (x_1 - x_0 + 1) * (x_0**2 + x_1**2 + 2) // 2
 
     def count_lattice_points_under_parabola(self, x_0, x_1):
-        if x_0 >= 0:
-            return x_1 * (x_1 + 1) * (2 * x_1 + 1) // 6 - x_0 * (x_0 - 1) * (2 * x_0 - 1) // 6
-        elif x_1 >= 0:
-            return x_1 * (x_1 + 1) * (2 * x_1 + 1) // 6 - x_0 * (-x_0 + 1) * (-2 * x_0 + 1) // 6
-        else:
-            return self.count_lattice_points_under_parabola(-x_1, -x_0)
+        if x_0 >= 0: # 0 <= x_0 <= x_1
+            return self.sigma_2(x_1) - self.sigma_2(x_0 - 1)
+        elif x_1 >= 0: # x_0 < 0 <= x_1
+            return self.sigma_2(x_1) + self.sigma_2(-x_0)
+        else: # x_0 <= x_1 < 0 
+            return self.sigma_2(-x_0) - self.sigma_2(-x_1 - 1)
 
+    def sigma_list(self, first, last):
+        return [
+            self.sigma_0(last) - self.sigma_0(first - 1),
+            self.sigma_1(last) - self.sigma_1(first - 1),
+            self.sigma_2(last) - self.sigma_2(first - 1),
+            self.sigma_3(last) - self.sigma_3(first - 1),
+        ]
 
+    def sigma_0(self, n):
+        return n
+
+    def sigma_1(self, n):
+        return n * (n + 1) // 2
+
+    def sigma_2(self, n):
+        return n * (n + 1) * (2 * n + 1) // 6
+
+    def sigma_3(self, n):
+        return (n * (n + 1) // 2)**2
 
 def main():
     problem = Problem()
