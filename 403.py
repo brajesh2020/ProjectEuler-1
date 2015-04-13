8e49cdce17c536992a2b07bb69d651f7963c6f45
Meng-Gen Tsai <menggen@thermalmix-lm.(none)>
2015-04-13 14:06:16 +0800

    O(sqrt(N))

--- 403.py
+++ 403.py
@@ -3,43 +3,44 @@ import sys
 
 class Problem():
     def solve(self):
-        #self.get(5)
-        print([self.get(n) for n in range(1, 100 + 1)])
+        for n in [5, 100, 10**12]:
+            print(n, '=>', self.get(n))
 
     def get(self, n):
-        total_sum = 0
+        total_sum = self.count(0, 0, 0, n) 
+        print('x_0 = 0 =>', total_sum)
 
-        for x_1 in range(n + 1):
-            lattice_points = self.count_lattice_points(0, x_1)
-            total_sum += lattice_points
-
-        for x_1 in range(1, n):
-            lattice_points = self.count_lattice_points(1, x_1)
-            total_sum += lattice_points
+        total_sum += self.count(1, 1, 1, n - 1)
+        print('x_0 = 1 =>', total_sum)
 
         sqrt_n = int(math.sqrt(n))
         for x_0 in range(2, sqrt_n + 1):
-            for x_1 in range(x_0, n // x_0 + 1):
-                lattice_points = self.count_lattice_points(x_0, x_1)
-                total_sum += lattice_points
-
-        for x_0 in range(1, n + 1):
-            for x_1 in range(n // x_0 + 1):
-                lattice_points = self.count_lattice_points(-x_0, x_1)
-                total_sum += lattice_points
+            total_sum += self.count(x_0, x_0, x_0, n // x_0)
+        print('x_0 > 1 =>', total_sum)
 
         for x_0 in range(1, sqrt_n + 1):
-            for x_1 in range(1, x_0 + 1):
-                lattice_points = self.count_lattice_points(-x_0, -x_1)
-                total_sum += lattice_points
+            total_sum += self.count(-x_0, -x_0, 0, n // x_0)
+        print('x_0 < 0, x_1 > 0 with <= Sqrt[n] =>', total_sum)
 
-        for x_0 in range(sqrt_n + 1, n):
-            for x_1 in range(1, n // x_0 + 1):
-                lattice_points = self.count_lattice_points(-x_0, -x_1)
-                total_sum += lattice_points
-        #print('total_sum =>', total_sum)
+        for d in range(1, n // (sqrt_n + 1) + 1):
+            total_sum += self.count(-(n // d), -(n // (d + 1)) - 1, 0, d)
+        print('x_0 < 0, x_1 > 0 with > Sqrt[n] =>', total_sum)
+
+        for x_0 in range(1, sqrt_n + 1):
+            total_sum += self.count(-x_0, -x_0, -x_0, -1)
+        print('x_0 < 0, x_1 < 0 with <= Sqrt[n] =>', total_sum)
+
+        total_sum += self.count(-(n - 1), -(n // 2) - 1, -1, -1)
+        for d in range(2, n // (sqrt_n + 1) + 1):
+            total_sum += self.count(-(n // d), -(n // (d + 1)) - 1, -d, -1)
+        print('x_0 < 0, x_1 < 0 with > Sqrt[n] =>', total_sum)
+
         return total_sum
 
+    def count(self, x_0_first, x_0_last, x_1_first, x_1_last):
+        line = self.count_under_line(x_0_first, x_0_last, x_1_first, x_1_last)
+        parabola = self.count_under_parabola(x_0_first, x_0_last, x_1_first, x_1_last)
+        return line - parabola
 
     def count_under_line(self, x_0_first, x_0_last, x_1_first, x_1_last):
         x_0_sigma_list = self.sigma_list(x_0_first, x_0_last)
@@ -56,56 +57,16 @@ class Problem():
         return double_count // 2
 
     def count_under_parabola(self, x_0_first, x_0_last, x_1_first, x_1_last):
-        assert(x_0_last <= x_1_first)
         x_0_sigma_list = self.sigma_list(x_0_first, x_0_last)
         x_1_sigma_list = self.sigma_list(x_1_first, x_1_last)
-        if x_0_first >= 0:
-            six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
-                    + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
-                    + x_0_sigma_list[0] * x_1_sigma_list[1] \
-                    - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
-                    + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
-                    - x_0_sigma_list[1] * x_1_sigma_list[0]
-            return six_times_count // 6
-        elif x_0_last < 0:
-            if x_1_first >= 0:
-                six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
-                        + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
-                        + x_0_sigma_list[0] * x_1_sigma_list[1] \
-                        - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
-                        + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
-                        - x_0_sigma_list[1] * x_1_sigma_list[0]
-                return six_times_count // 6
-            elif x_1_last < 0:
-                six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
-                        + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
-                        + x_0_sigma_list[0] * x_1_sigma_list[1] \
-                        - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
-                        + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
-                        - x_0_sigma_list[1] * x_1_sigma_list[0]
-                return six_times_count // 6
-            else:
-                raise NotImplementedError()
-        else:
-            raise NotImplementedError()
-
-
-    def count_lattice_points(self, x_0, x_1):
-        line = self.count_lattice_points_under_line(x_0, x_1)
-        parabola = self.count_lattice_points_under_parabola(x_0, x_1)
-        return line - parabola
-
-    def count_lattice_points_under_line(self, x_0, x_1):
-        return (x_1 - x_0 + 1) * (x_0**2 + x_1**2 + 2) // 2
-
-    def count_lattice_points_under_parabola(self, x_0, x_1):
-        if x_0 >= 0: # 0 <= x_0 <= x_1
-            return self.sigma_2(x_1) - self.sigma_2(x_0 - 1)
-        elif x_1 >= 0: # x_0 < 0 <= x_1
-            return self.sigma_2(x_1) + self.sigma_2(-x_0)
-        else: # x_0 <= x_1 < 0 
-            return self.sigma_2(-x_0) - self.sigma_2(-x_1 - 1)
-
+        six_times_count = 2 * x_0_sigma_list[0] * x_1_sigma_list[3] \
+                + 3 * x_0_sigma_list[0] * x_1_sigma_list[2] \
+                + x_0_sigma_list[0] * x_1_sigma_list[1] \
+                - 2 * x_0_sigma_list[3] * x_1_sigma_list[0] \
+                + 3 * x_0_sigma_list[2] * x_1_sigma_list[0] \
+                - x_0_sigma_list[1] * x_1_sigma_list[0]
+        return six_times_count // 6
+        
     def sigma_list(self, first, last):
         return [
             self.sigma_0(last) - self.sigma_0(first - 1),
