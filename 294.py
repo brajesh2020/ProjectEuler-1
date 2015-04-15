3d32bb4980d62a11e1065d54cb7b5c02de8519d6
Meng-Gen Tsai <menggen@thermalmix-lm.(none)>
2015-04-15 11:20:17 +0800

    Refactor

--- 294.py
+++ 294.py
@@ -19,49 +19,36 @@ class Problem():
     def count(self, n):
         if n == 1:
             return self.one_map
-        
-        half_count_map = self.count(n // 2)
-
-        delta = pow(10, n // 2, 23)        
-        double_count_map = [{} for _ in range(23 + 1)]
-        for i in range(23 + 1):
-            for j in range(23 - i + 1):
-                left_count_map = half_count_map[i]
-                right_count_map = half_count_map[j]
-                for left_key in left_count_map:
-                    left_count = left_count_map[left_key]
-                    for right_key in right_count_map:
-                        right_count = right_count_map[right_key]
-                        key = (left_key * delta + right_key) % 23
-                        if key not in double_count_map[i + j]:
-                            double_count_map[i + j][key] = 0
-                        double_count_map[i + j][key] = (double_count_map[i + j][key] + left_count * right_count) % self.mod
-        if n & 1 == 0:
+        else:
+            half_count_map = self.count(n // 2)
+            count_map = self.__merge(half_count_map, half_count_map, pow(10, n // 2, 23))
+            if n % 2 == 1:
+                count_map = self.__merge(count_map, self.one_map, 10)
             print('current n =>', n)
-            return double_count_map
-
-        count_map = [{} for _ in range(23 + 1)]
-        for i in range(23 + 1):
-            for j in range(23 - i + 1):
-                left_count_map = double_count_map[i]
-                right_count_map = self.one_map[j]
-                for left_key in left_count_map:
-                    left_count = left_count_map[left_key]
-                    for right_key in right_count_map:
-                        right_count = right_count_map[right_key]
-                        key = (left_key * 10 + right_key) % 23
-                        if key not in count_map[i + j]:
-                            count_map[i + j][key] = 0
-                        count_map[i + j][key] = (count_map[i + j][key] + left_count * right_count) % self. mod
-        print('current n =>', n)
-        return count_map
-
+            return count_map
+            
     def __init_one_map(self):
         one_map = [{} for _ in range(23 + 1)]
         for digit in range(10):
             digit_sum = digit
             one_map[digit][digit] = 1
         return one_map
+
+    def __merge(self, left_count_map, right_count_map, delta):
+        result = [{} for _ in range(23 + 1)]
+        for i in range(23 + 1):
+            for j in range(23 - i + 1):
+                left = left_count_map[i]
+                right = right_count_map[j]
+                for left_key in left:
+                    left_count = left[left_key]
+                    for right_key in right:
+                        right_count = right[right_key]
+                        key = (left_key * delta + right_key) % 23
+                        if key not in result[i + j]:
+                            result[i + j][key] = 0
+                        result[i + j][key] = (result[i + j][key] + left_count * right_count) % self.mod
+        return result
 
 def main():
     problem = Problem()
