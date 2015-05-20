f06127a6436b6d1857b90b6febd825dc7f1d2222
Meng-Gen <plover@gmail.com>
2015-05-20 23:56:25 +0800

    Dynamic programming

--- 290.py
+++ 290.py
@@ -4,13 +4,13 @@ class Problem():
     def __init__(self):
         self.digit_signature = [(137 * d) % 10 for d in range(10)]
         self.digit_carry = [(137 * d) // 10 for d in range(10)]
-        #print(self.digit_signature)
-        #print(self.digit_carry)
 
     def solve(self):
-        for n in [18]:
-            #self.get_bruteforce(10**n)
-            self.get(n)
+        #self.benchmark()
+        print(self.get(18))
+
+    def benchmark(self):
+        assert(self.get(5) == self.get_bruteforce(10**5))
 
     def get(self, n):
         diff_map = { 0 : { 0 : 1 } }
@@ -29,28 +29,23 @@ class Problem():
                         if new_carry not in new_diff_map[new_diff]:
                             new_diff_map[new_diff][new_carry] = 0
                         new_diff_map[new_diff][new_carry] += diff_map[diff][carry]
-                        #print(new_signature, new_diff, new_carry)
             diff_map = new_diff_map
-        print(self.__get_count(diff_map))
-            #print(new_diff_map)
+        return self.__get_count(diff_map)
 
     def __get_count(self, diff_map):
-        #print(diff_map)
         count = 0
         for diff in diff_map:
             for carry in diff_map[diff]:
                 if diff == self.get_digit_sum(carry):
                     count += diff_map[diff][carry]
-                    #print(diff, carry, diff_map[diff][carry])
         return count
 
     def get_bruteforce(self, n):
         count = 0
         for i in range(n):
             if self.get_digit_sum(i) == self.get_digit_sum(137 * i):
-                #print(i)
                 count += 1
-        print(count)
+        return count
 
     def get_digit_sum(self, n):
         result = 0
